b8494f94502807613b43b97c8cf8ef20ba47b7c0
naaperusrinu <srinua6@gmail.com>
2025-04-24 10:10:27 +0800

    first commit

---
+++ sample.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World! this is Sample');
+}).listen(8080);
